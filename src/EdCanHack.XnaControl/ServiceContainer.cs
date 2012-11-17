54fac88d2770cd0f785ebe19194688b0ef418c42
Ed Ropple <ed@edropple.com>
2012-11-17 16:01:12 -0500

    seed commit

---
+++ src/EdCanHack.XnaControl/ServiceContainer.cs
@@ -0,0 +1,52 @@
+﻿#region File Description
+// -------------------------------------------------------------------------------------------------
+// Copyright (C) 2012 Ed Ropple <ed+xnacontrol@edropple.com>
+//
+// Derivative of works Copyright (C) Microsoft Corporation.
+// Original project: http://xbox.create.msdn.com/en-US/education/catalog/sample/winforms_series_1
+//
+// Released under the Microsoft Public License: http://opensource.org/licenses/MS-PL
+// -------------------------------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace EdCanHack.XnaControl
+{
+    /// <summary>
+    /// Container class implements the IServiceProvider interface. This is used
+    /// to pass shared services between different components, for instance the
+    /// ContentManager uses it to locate the IGraphicsDeviceService implementation.
+    /// </summary>
+    public class ServiceContainer : IServiceProvider
+    {
+        readonly Dictionary<Type, Object> _services = new Dictionary<Type, Object>();
+
+
+        /// <summary>
+        /// Adds a new service to the collection.
+        /// </summary>
+        public void AddService<T>(T service)
+        {
+            _services.Add(typeof(T), service);
+        }
+
+
+        /// <summary>
+        /// Looks up the specified service.
+        /// </summary>
+        public object GetService(Type serviceType)
+        {
+            object service;
+
+            _services.TryGetValue(serviceType, out service);
+
+            return service;
+        }
+    }
+}
